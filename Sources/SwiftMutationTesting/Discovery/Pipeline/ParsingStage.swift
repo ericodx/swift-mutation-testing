bf3d79716083a3c6f8109ae620fcbf033b4d3eea
Érico Perez Neto <ericodx@icloud.com>
2026-03-19 21:21:02 -0300

    feat: add ParsingStage

---
+++ Sources/SwiftMutationTesting/Discovery/Pipeline/ParsingStage.swift
@@ -0,0 +1,24 @@
+import SwiftParser
+
+struct ParsingStage {
+    func run(sourceFiles: [SourceFile]) async -> [ParsedSource] {
+        await withTaskGroup(of: ParsedSource.self) { group in
+            for file in sourceFiles {
+                group.addTask {
+                    ParsedSource(
+                        file: file,
+                        syntax: Parser.parse(source: file.content)
+                    )
+                }
+            }
+
+            var results: [ParsedSource] = []
+
+            for await parsed in group {
+                results.append(parsed)
+            }
+
+            return results
+        }
+    }
+}
