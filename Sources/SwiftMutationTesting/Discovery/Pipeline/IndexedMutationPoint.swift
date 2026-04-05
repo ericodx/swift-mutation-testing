15f6805a9c4b4aa1d913a6469dce47aba03a5b93
Érico Perez Neto <ericodx@icloud.com>
2026-04-05 00:48:21 -0300

    refactor: extract toDescriptor into IndexedMutationPoint

--- Sources/SwiftMutationTesting/Discovery/Pipeline/IndexedMutationPoint.swift
+++ Sources/SwiftMutationTesting/Discovery/Pipeline/IndexedMutationPoint.swift
@@ -2,4 +2,25 @@ struct IndexedMutationPoint: Sendable {
     let index: Int
     let mutation: MutationPoint
     let isSchematizable: Bool
+
+    var mutantID: String {
+        "swift-mutation-testing_\(index)"
+    }
+
+    func toDescriptor(mutatedContent: String?) -> MutantDescriptor {
+        MutantDescriptor(
+            id: mutantID,
+            filePath: mutation.filePath,
+            line: mutation.line,
+            column: mutation.column,
+            utf8Offset: mutation.utf8Offset,
+            originalText: mutation.originalText,
+            mutatedText: mutation.mutatedText,
+            operatorIdentifier: mutation.operatorIdentifier,
+            replacementKind: mutation.replacement,
+            description: mutation.description,
+            isSchematizable: isSchematizable,
+            mutatedSourceContent: mutatedContent
+        )
+    }
 }
