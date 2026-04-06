47d6418c0bbdc3623349178a9038d0438d423fcd
Érico Perez Neto <ericodx@icloud.com>
2026-04-06 16:52:49 -0300

    feat: add killerTestFile to ExecutionResult and ExecutionDeps

--- Sources/SwiftMutationTesting/Execution/ExecutionResult.swift
+++ Sources/SwiftMutationTesting/Execution/ExecutionResult.swift
@@ -1,5 +1,14 @@
 struct ExecutionResult: Sendable, Codable {
+
+    init(descriptor: MutantDescriptor, status: ExecutionStatus, testDuration: Double, killerTestFile: String? = nil) {
+        self.descriptor = descriptor
+        self.status = status
+        self.testDuration = testDuration
+        self.killerTestFile = killerTestFile
+    }
+
     let descriptor: MutantDescriptor
     let status: ExecutionStatus
     let testDuration: Double
+    let killerTestFile: String?
 }
